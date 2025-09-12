658bb4aef6662e84d868ff898b1d5adcd1973ee0
sakiyamaK <sakiyama.k@gmail.com>
2025-09-12 21:11:20 +0900

    キーボードを閉じる

--- iOSEngineerCodeCheck/SearchGitHubList/ViewController/SearchGitHubListViewController.swift
+++ iOSEngineerCodeCheck/SearchGitHubList/ViewController/SearchGitHubListViewController.swift
@@ -119,6 +119,7 @@ extension SearchGitHubListViewController: UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
         Task {
             do {
                 try await viewModel.search(text: searchBar.text)
